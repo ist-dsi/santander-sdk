519c3a989178ccc11a69d8009b7d49f62dbb70b8
Francisco Neves <francisco.neves@tecnico.ulisboa.pt>
2019-03-28 09:28:50 +0000

    Fix package name

--- src/main/java/org/fenixedu/bennu/SantanderSdkSpringConfiguration.java
+++ src/main/java/org/fenixedu/bennu/SantanderSdkSpringConfiguration.java
@@ -5,7 +5,7 @@ import org.fenixedu.commons.configuration.ConfigurationInvocationHandler;
 import org.fenixedu.commons.configuration.ConfigurationManager;
 import org.fenixedu.commons.configuration.ConfigurationProperty;
 
-@BennuSpringModule(basePackages = "org.fenixedu.santander-sdk", bundles = "SantanderSdkResources")
+@BennuSpringModule(basePackages = "org.fenixedu.santandersdk", bundles = "SantanderSdkResources")
 public class SantanderSdkSpringConfiguration {
 
     @ConfigurationManager(description = "Identification Cards Configuration")
@@ -20,15 +20,6 @@ public class SantanderSdkSpringConfiguration {
                 key = "sibs.webService.password",
                 description = "Password used to communicate with the SIBS Web Service, which returns the ID card production state.")
         String sibsWebServicePassword();
-
-        @ConfigurationProperty(key = "app.institution.AES128.secretKey",
-                description = "Secret for Institution ID card generation", defaultValue = "aa0bbfaf79654df4")
-        String appInstitutionAES128SecretKey();
-
-        @ConfigurationProperty(key = "app.institution.PIN", description = "PIN for Institution ID card generation",
-                defaultValue = "0000")
-        String appInstitutionPIN();
-
     }
 
     public static ConfigurationProperties getConfiguration() {
