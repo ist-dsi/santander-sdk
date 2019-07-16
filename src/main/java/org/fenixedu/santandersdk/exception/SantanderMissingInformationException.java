959ae4a8790e80d7bb319820f72b42b9030b30f8
João Abrantes <17602797+JCAbrantes@users.noreply.github.com>
2019-07-16 17:42:35 +0100

    Add new Exception for when user information is missing (#5)

---
+++ src/main/java/org/fenixedu/santandersdk/exception/SantanderMissingInformationException.java
@@ -0,0 +1,13 @@
+package org.fenixedu.santandersdk.exception;
+
+public class SantanderMissingInformationException extends SantanderValidationException {
+
+    public SantanderMissingInformationException() {
+        super();
+    }
+
+    public SantanderMissingInformationException(String msg) {
+        super(msg);
+    }
+
+}
